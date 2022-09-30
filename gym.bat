503640d66f38189c2a728e15270fe2e273993540
Sergio <kirienkosergio@gmail.com>
2022-09-30 16:34:52 +0200

    upload batfile

---
+++ gym.bat
@@ -0,0 +1,3 @@
+cd C:\Users\a2504\Desktop\gym\src
+javac --module-path "%PATH_TO_FX%\lib" --add-modules javafx.base,javafx.controls,javafx.fxml Main.java
+java --module-path "%PATH_TO_FX%\lib" --add-modules javafx.base,javafx.controls,javafx.fxml Main.java
